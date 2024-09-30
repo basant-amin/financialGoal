8bac2eb2bbc7c91a343c850114c823f4b4477015
basant <basantamin@outlook.sa>
2024-09-30 12:28:49 +0300

    Initial Commit

---
+++ financialGoal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  financialGoal
+//
+//  Created by basant amin bakir on 30/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ financialGoal/financialGoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  financialGoalApp.swift
+//  financialGoal
+//
+//  Created by basant amin bakir on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct financialGoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
