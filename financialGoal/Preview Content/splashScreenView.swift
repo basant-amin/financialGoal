712e6ac31e67c25b6df9dffbfc05d150795a447f
Raghad0IT <raghadtuwaiq@gmail.com>
2024-10-02 17:05:57 +0300

    add logo on splashscreen

--- financialGoal/Preview Content/splashScreenView.swift
+++ financialGoal/Preview Content/splashScreenView.swift
@@ -4,58 +4,60 @@
 //
 //  Created by basant amin bakir on 30/09/2024.
 //
-
 import SwiftUI
 
-struct splashScreenView: View {
+struct SplashScreenView: View {
     @State private var viewContent = false
-    @State private var size = 0.8
-    @State private var opacity = 0.5
+    @State private var size: CGFloat = 0.8
+    @State private var opacity: Double = 0.5
+    @State private var rotation: Double = 0.0
     
     var body: some View {
-        
         if viewContent {
             ContentView()
-        }else{
-            
+        } else {
             VStack {
-                VStack{
-                    Image(systemName: "dollarsign.circle.fill")
-                        .font(.system(size: 80))
-                        .foregroundColor(.yellow)
-                    Text("Financial Goal")
-                        .font(.system(size: 26))
-                        .foregroundColor(.black.opacity(0.80))
-                    
-                         
-                }
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeIn(duration: 1.2)){
-                        self.size = 0.9
+                Image("IOSlogo") // Replace with your image name
+                    .resizable()
+                    .aspectRatio(contentMode: .fit) // Adjusts the image size to fit
+                    .frame(width: 400, height: 400) // Set the size of the image
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .rotationEffect(.degrees(rotation)) // Add rotation effect
+                    .onAppear {
+                        withAnimation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) {
+                            self.size = 1.0 // Scale to original size
+                            self.opacity = 1.0 // Fade in
+                            self.rotation = 360 // Full rotation
+                        }
+
+                        // Adding a scale down effect before scaling up
+                        withAnimation(.easeOut(duration: 0.5)) {
+                            self.size = 0.5 // Scale down initially
+                        }
+
+                        // Then scale back up
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            withAnimation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) {
+                                self.size = 1.0 // Scale back to original size
+                                self.opacity = 1.0 // Fade in
+                                self.rotation = 360 // Full rotation
+                            }
+                        }
+
+                    }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    withAnimation {
+                        self.viewContent = true
                     }
                 }
             }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now()+2.0){
-                    
-                    withAnimation{
-                        self.viewContent = true
-                    }
-                  
-                }
-            
-        }
-        
-        
-        
-  
         }
     }
 }
 
-
 #Preview {
-    splashScreenView()
+    SplashScreenView()
 }
--- financialGoal/financialGoalApp.swift
+++ financialGoal/financialGoalApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct financialGoalApp: App {
     var body: some Scene {
         WindowGroup {
-            splashScreenView()
+            SplashScreenView()
         }
     }
 }
